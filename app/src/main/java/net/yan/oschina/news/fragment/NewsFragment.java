7b2f23947ded677b67ecdc0fb4db17e01b74860e
zwt7 <2992532405@qq.com>
2019-12-13 08:28:21 +0800

    软件的布局

--- app/src/main/java/net/yan/oschina/news/fragment/NewsFragment.java
+++ app/src/main/java/net/yan/oschina/news/fragment/NewsFragment.java
@@ -20,42 +20,39 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class NewsFragment extends Fragment {
-
-    @BindView(R.id.viewpager)
-    ViewPager viewPager;
-    @BindView(R.id.viewPagerTab)
-    SlidingTabLayout viewPagerTab;
-
-    private ArrayList<Fragment> mFragments = new ArrayList<>();
-    private final String[] mTitles = {"关注", "软件", "资讯", "推荐", "问答", "博客", "英文"};
-
+    @BindView(R.id.tl_1)
+    SlidingTabLayout tab;
+    @BindView(R.id.vp)
+    ViewPager pager;
     private Unbinder binder;
+
+    private ArrayList<Fragment> mFragment=new ArrayList<>();
+    private final String[] mTitles={"关注","软件","资讯","推荐","问答","博客","英文"};
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_news, container, false);
-        binder = ButterKnife.bind(this, view);
+        View view=inflater.inflate(R.layout.fragment_news,container,false);
+        binder=ButterKnife.bind(this,view);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mFragments.add(new FollowFragment());
-        mFragments.add(new SoftwareFragment());
-        mFragments.add(new InformationFragment());
-        mFragments.add(new RecommendFragment());
-        mFragments.add(new QuestionFragment());
-        mFragments.add(new BlogFragment());
-        mFragments.add(new EnglishFragment());
-
-        viewPagerTab.setViewPager(viewPager,mTitles,getActivity(),mFragments);
+        mFragment.add(new FocusFragment());
+        mFragment.add(new SoftwareFragment());
+        mFragment.add(new InformationFragment());
+        mFragment.add(new RecommendFragment());
+        mFragment.add(new QuestionFragment());
+        mFragment.add(new BlogFragment());
+        mFragment.add(new EnglishFragment());
+        tab.setViewPager(pager,mTitles,getActivity(),mFragment);
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
+    public void onDestroy() {
+        super.onDestroy();
         binder.unbind();
     }
 }
--- app/src/main/java/net/yan/oschina/news/fragment/RecommendFragment.java
+++ app/src/main/java/net/yan/oschina/news/fragment/RecommendFragment.java
@@ -1,21 +1,6 @@
 package net.yan.oschina.news.fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import net.yan.oschina.R;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class RecommendFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recommend,container,false);
-        return view;
-    }
 }
