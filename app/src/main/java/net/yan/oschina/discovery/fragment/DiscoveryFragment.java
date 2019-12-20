01618d35e9ec06561dbb4f899d47ae70f55e276a
zhangq <you@example.com>
2019-12-20 15:12:29 +0800

    发现

--- app/src/main/java/net/yan/oschina/discovery/fragment/DiscoveryFragment.java
+++ app/src/main/java/net/yan/oschina/discovery/fragment/DiscoveryFragment.java
@@ -1,6 +1,21 @@
 package net.yan.oschina.discovery.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.yan.oschina.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class DiscoveryFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_discover,container,false);
+        return view;
+    }
 }
