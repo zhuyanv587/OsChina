0ba388ee5caaf6de962cfe63ebfc372a63c5e0e6
zhangq <you@example.com>
2019-12-20 09:01:31 +0800

    我的

--- app/src/main/java/net/yan/oschina/tweet/fragment/MeFragment.java
+++ app/src/main/java/net/yan/oschina/tweet/fragment/MeFragment.java
@@ -19,7 +19,7 @@ public class MeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_me, container, false);
+        View view = inflater.inflate(R.layout.fragment_me2, container, false);
         binder = ButterKnife.bind(this, view);
         return view;
     }
