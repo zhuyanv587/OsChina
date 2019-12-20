35dd51a647d1ce72068f5f9530d59f88dec75a08
zhuyanv587 <1677391824@qq.com>
2019-12-20 13:16:19 +0800

    综合中的搜索功能的跳转

---
+++ app/src/main/java/net/yan/oschina/news/activity/SearchActivity.java
@@ -0,0 +1,16 @@
+package net.yan.oschina.news.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.yan.oschina.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
