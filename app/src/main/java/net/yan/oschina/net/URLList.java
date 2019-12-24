f2e1c857c081d771c8338c0141eb82a0fc40a906
zhuyanv587 <1677391824@qq.com>
2019-12-24 12:17:17 +0800

    乱弹网络功能实现

--- app/src/main/java/net/yan/oschina/net/URLList.java
+++ app/src/main/java/net/yan/oschina/net/URLList.java
@@ -11,4 +11,5 @@ public class URLList {
     public final static String GET_SOFTWARE = domain + "/action/openapi/project_tag_list?access_token=";
     public final static String SEND_TWEET = domain+ "/action/openapi/tweet_pub";
     public final static String SEND_QUESTION = domain+"/action/openapi/post_pub";
+    public final static String GET_THRUM = domain + "/action/openapi/tweet_list?access_token=";
 }
--- app/src/main/java/net/yan/oschina/tweet/adapter/ThrumAdapter.java
+++ app/src/main/java/net/yan/oschina/tweet/adapter/ThrumAdapter.java
@@ -3,6 +3,7 @@ package net.yan.oschina.tweet.adapter;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 
+import net.yan.oschina.R;
 import net.yan.oschina.tweet.entity.Thrum;
 
 import java.util.List;
@@ -17,6 +18,10 @@ public class ThrumAdapter extends BaseQuickAdapter<Thrum, BaseViewHolder> {
 
     @Override
     protected void convert(@NonNull BaseViewHolder helper, Thrum item) {
-
+        helper.setText(R.id.thrum_title, item.getId() + "");
+        helper.setText(R.id.thrum_content, item.getBody());
+        helper.setText(R.id.thrum_author, item.getAuthor());
+        helper.setText(R.id.thrum_time, item.getPubDate());
+        helper.setText(R.id.thrum_num, item.getCommentCount() + "");
     }
 }
